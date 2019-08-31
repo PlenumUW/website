9105180d4d3bf293e65f6b6dc913edd9de4af4cd
Jackson Baker Ryan <jackryan@uw.edu>
2019-08-30 20:32:11 -0700

    Init prerender

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 /**
  * Returns the path to the specified directory relative to the root directory
@@ -7,27 +9,13 @@ function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
-/*
-  Prismic prerender build
-*/
-const BuildUtil = require("./prerender.build.js");
-
-/*
-  Webpack Exports
-*/
 module.exports = {
-  runtimeCompiler: false, // One prerender guide set to true
-  productionSourceMap: false, // TODO: should be on, 'terser' dependency is buggy
-
   // Add scss variables in _settings.scss to scss global scope
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "scss",
       patterns: ["./src/styles/globals.scss"]
     }
-  },
-  configureWebpack: {
-    plugins: BuildUtil.plugins
   },
   chainWebpack: config => {
     // Create environment variable of the build date
@@ -52,20 +40,85 @@ module.exports = {
       .test(/inline\.(.*)\.svg/)
       .use("vue-svg-loader")
       .loader("vue-svg-loader");
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: resolve("dist"),
+        outputDir: resolve("dist"),
+        routes: ["/", "/about", "/test"],
+
+        renderer: new Renderer({
+          inject: {
+            foo: "bar"
+          },
+          headless: true,
+          renderAfterDocumentEvent: "render-event"
+        })
+      })
+    ]
   }
-
-  // configureWebpack: async config => {
-  //   if (process.env.BUILD_OPTION === "prerender") {
-  //     // Set within .env file of prerender mode
-  //     let plugins;
-
-  //     try {
-  //       plugins = await BuildUtil.getProductionPlugins();
-  //     } catch (err) {
-  //       throw err;
-  //     }
-
-  //     (config.plugins || []).concat(...plugins);
-  //   }
-  // }
 };
+
+/*
+  Prismic prerender build
+*/
+// const BuildUtil = require("./prerender.build.js");
+
+/*
+  Webpack Exports
+*/
+// module.exports = {
+//   runtimeCompiler: false, // One prerender guide set to true
+//   productionSourceMap: false, // TODO: should be on, 'terser' dependency is buggy
+
+//   // Add scss variables in _settings.scss to scss global scope
+//   pluginOptions: {
+//     "style-resources-loader": {
+//       preProcessor: "scss",
+//       patterns: ["./src/styles/globals.scss"]
+//     }
+//   },
+//   configureWebpack: {
+//     plugins: BuildUtil.plugins
+//   },
+//   chainWebpack: config => {
+//     // Create environment variable of the build date
+//     config.plugin("define").tap(definitions => {
+//       definitions[0]["process.env"][
+//         "BUILD_DATE_ISO"
+//       ] = new Date().toISOString();
+
+//       return definitions;
+//     });
+
+//     // Resolve file references that start with "assets"
+//     config.resolve.alias.set("assets", resolve("src/assets"));
+
+//     // Allow inline svgs and style tag svgs, both in parallel
+//     const svgRule = config.module.rule("svg");
+//     const inlineSvgRule = config.module.rule("inline-svg");
+
+//     svgRule.exclude.add(/inline\.(.*)\.svg/).end();
+
+//     inlineSvgRule
+//       .test(/inline\.(.*)\.svg/)
+//       .use("vue-svg-loader")
+//       .loader("vue-svg-loader");
+//   }
+
+//   // configureWebpack: async config => {
+//   //   if (process.env.BUILD_OPTION === "prerender") {
+//   //     // Set within .env file of prerender mode
+//   //     let plugins;
+
+//   //     try {
+//   //       plugins = await BuildUtil.getProductionPlugins();
+//   //     } catch (err) {
+//   //       throw err;
+//   //     }
+
+//   //     (config.plugins || []).concat(...plugins);
+//   //   }
+//   // }
+// };

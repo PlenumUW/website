2d7095cd0c804bbff2c64aff5ba2b3034c98e198
Jackson Baker Ryan <jackryan@uw.edu>
2019-09-04 14:07:45 -0700

    Init home issue slice

---
+++ src/components/slices/home/IssueSlice.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="c-issue-slice">
+    <img
+      class="c-issue-slice__cover-image"
+      src="http://placehold.jp/250x325.png?text=Issue-Cover-Image"
+    />
+    <div class="c-issue-slice__toc"></div>
+  </section>
+</template>
+<script>
+export default {
+  name: "IssueSlice",
+  components: {},
+  props: {}
+};
+</script>
+<style lang="scss" scoped>
+.c-issue-slice {
+  position: relative;
+  padding-right: 70px;
+
+  &__cover-image {
+    width: 100%;
+    height: auto;
+  }
+
+  &__toc {
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,23 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <issue-slice class="slice"></issue-slice>
+  </div>
 </template>
 
 <script>
+import IssueSlice from "@/components/slices/home/IssueSlice";
+
 export default {
   name: "home",
-  components: {}
+  components: { IssueSlice }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  padding-left: 133px;
+  background: rgba(255, 192, 203, 0.466);
+}
+.slice {
+}
+</style>

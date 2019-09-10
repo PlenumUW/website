15e74ed52c5398770f63eda7dca6d8b7576124cc
Jackson Baker Ryan <jackryan@uw.edu>
2019-09-10 11:19:50 -0700

    Init issue view component

---
+++ src/views/Issue.vue
@@ -0,0 +1,12 @@
+<template>
+  <article></article>
+</template>
+<script>
+import View from "./View";
+
+export default {
+  name: "issue",
+  extends: View
+};
+</script>
+<style lang="scss" scoped></style>

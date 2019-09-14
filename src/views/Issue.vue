8b3c66ff81d73bfce2cb2352e14acc8bf9ca7bf3
Jackson Baker Ryan <jackryan@uw.edu>
2019-09-14 13:34:22 -0700

    Init issue view population with api data

--- src/views/Issue.vue
+++ src/views/Issue.vue
@@ -1,39 +1,52 @@
 <template>
   <article>
-    <paper :color="color">{{ title }}</paper>
+    <paper :color="color">
+      <h1>{{ title }}</h1>
+      <img :src="image.url" />
+    </paper>
   </article>
 </template>
 <script>
 import BaseView from "./BaseView";
 
+// TODO: make DRY with IssueSlice
+
 export default {
   name: "Issue",
   extends: BaseView,
+  data: function() {
+    return {
+      issue: undefined
+    };
+  },
   computed: {
     title: function() {
-      return "Issue Title";
+      if (!this.issue) return "";
+
+      return this.PrismicProcessor.getRawText(this.issue.title);
     },
-    description: function() {
-      return "Description of the Issue";
-    },
-    imgSrc: function() {
-      return "http://placehold.jp/80/bfbfbf/ffffff/1400x1800.png?text=Issue Cover Image";
-    },
-    metadata: function() {
-      return {
-        title: this.title,
-        description: this.description,
-        image: {
-          url: this.imgSrc,
-          dimensions: {
-            width: 1400,
-            height: 1800
-          },
-          alt: "Alt text"
-        },
-        authors: "Plenum - Undergraduate Journal of Geography"
-      };
+    image: function() {
+      if (!this.issue) return "";
+
+      return this.issue.cover_image;
     }
+  },
+  created: async function() {
+    const issue = await this.$api.fetchIssueBySlug(
+      this.$route.params.issueSlug
+    );
+
+    if (!this.docExists(issue)) {
+      return;
+    }
+
+    this.issue = issue;
+
+    this.metadata = {
+      title: this.title,
+      description: this.PrismicProcessor.getRawText(issue.description),
+      image: issue.cover_image.SocialMedia
+    };
   },
   meta() {
     return this.MetadataManager.metaDefault(this.metadata, "book");

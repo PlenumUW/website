8c6221e68d9816fbeef2b85d6bf852489a6bf942
Jackson Baker Ryan <jackryan@uw.edu>
2019-09-14 22:34:54 -0700

    Add rich text processing for page views - Add prismic rich text component   - Constructed to not require client-side runtime

---
+++ src/components/prismic/RichText.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="c-rich-text" v-html="html"></div>
+</template>
+
+<script>
+function htmlClassSyntax(classes) {
+  return `class="${classes}"`;
+}
+
+const Elements = {
+  pre: {
+    type: "preformatted",
+    open: ({ text } = {}, classes = "") => {
+      console.log(text, classes);
+      const firstClosingCaretIndex = text.indexOf(">");
+
+      let html =
+        text.substring(0, firstClosingCaretIndex) +
+        " " +
+        htmlClassSyntax(classes) +
+        text.substring(firstClosingCaretIndex);
+      console.log(html);
+      return html;
+    },
+    close: () => ""
+  },
+  p: {
+    type: "paragraph",
+    open: (data = {}, classes = "") => `<p ${htmlClassSyntax(classes)}>`,
+    close: () => "</p>"
+  },
+  // TODO: generate ImageSimple component
+  img: {
+    type: "image",
+    open: (data = {}, classes = "", alt = "", src = "") =>
+      `<img src="${src}" ${htmlClassSyntax(classes)} alt=${alt}>`,
+    close: () => "</img>"
+  },
+  a: {
+    type: "hyperlink",
+    open: ({ url, target } = {}, classes = "") =>
+      `<a ${htmlClassSyntax(classes)} href="${url}" target="${target}">`,
+    close: () => "</a>"
+  },
+  strong: {
+    type: "strong",
+    open: (data = {}, classes = "") => `<strong ${htmlClassSyntax(classes)}>`,
+    close: () => "</strong>"
+  },
+  em: {
+    type: "em",
+    open: (data = {}, classes = "") => `<em ${htmlClassSyntax(classes)}>`,
+    close: () => "</em>"
+  },
+  ulli: {
+    type: "list-item",
+    open: (data = {}, classes = "") => `<li ${htmlClassSyntax(classes)}>`,
+    close: () => "</li>"
+  },
+  olli: {
+    type: "o-list-item",
+    open: (data = {}, classes = "") => `<li ${htmlClassSyntax(classes)}>`,
+    close: () => "</li>"
+  },
+  ul: {
+    type: "u-list",
+    open: (data = {}, classes = "") => `<ul ${htmlClassSyntax(classes)}>`,
+    close: () => "</ul>"
+  },
+  ol: {
+    type: "o-list",
+    open: (data = {}, classes = "") => `<ol ${htmlClassSyntax(classes)}>`,
+    close: () => "</ol>"
+  },
+  h1: {
+    type: "heading1",
+    open: (data = {}, classes = "") => `<h1 ${htmlClassSyntax(classes)}>`,
+    close: () => "</h1>"
+  },
+  h2: {
+    type: "heading2",
+    open: classes => `<h2 ${htmlClassSyntax(classes + " h2")}>`,
+    close: () => "</h2>"
+  },
+  h3: {
+    type: "heading3",
+    open: classes => `<h3 ${htmlClassSyntax(classes + " h3")}>`,
+    close: () => "</h3>"
+  },
+  h4: {
+    type: "heading4",
+    open: classes => `<h4 ${htmlClassSyntax(classes + " h4")}>`,
+    close: () => "</h4>"
+  },
+  h5: {
+    type: "heading5",
+    open: classes => `<h5 ${htmlClassSyntax(classes + " h5")}>`,
+    close: () => "</h5>"
+  },
+  h6: {
+    type: "heading6",
+    open: classes => `<h6 ${htmlClassSyntax(classes + " h6")}>`,
+    close: () => "</h6>"
+  }
+};
+
+export default {
+  name: "RichText",
+  props: {
+    body: {
+      required: true,
+      type: Array,
+      /**
+       * Validates that the given objects are valid Prismic objects.
+       */
+      validator: function(val) {
+        return val.every(p => {
+          const hasProps =
+            p.hasOwnProperty("type") &&
+            ((p.hasOwnProperty("spans") && p.hasOwnProperty("text")) ||
+              (p.hasOwnProperty("alt") &&
+                p.hasOwnProperty("dimensions") &&
+                p.hasOwnProperty("copyright") &&
+                p.hasOwnProperty("url")));
+
+          if (!hasProps) return false;
+
+          const correctTypes =
+            Array.isArray(p.spans) &&
+            typeof p.text === "string" &&
+            typeof p.type === "string";
+
+          if (!correctTypes) return false;
+
+          return true;
+        });
+      }
+    },
+    classes: {
+      required: false,
+      type: String,
+      default: function() {
+        return "";
+      }
+    }
+  },
+  data: function() {
+    return {
+      olListOpen: false,
+      ulListOpen: false
+    };
+  },
+  computed: {
+    html: function() {
+      return this.buildHtml();
+    },
+    // Whether there is a wrapping element tag that is currently open
+    wrappingHtmlElementOpen: function() {
+      return this.olListOpen || this.ulListOpen;
+    }
+  },
+  methods: {
+    buildHtml() {
+      return this.body.reduce(
+        (html, element, index) => (html += this.serialize(element, index)),
+        ""
+      );
+    },
+    findElement(type) {
+      const elKey = Object.keys(Elements).find(key => {
+        return Elements[key].type === type;
+      });
+
+      if (elKey === undefined) {
+        throw new Error("Prismic element type, '" + type + "', unhandled.");
+      }
+
+      return Elements[elKey];
+    },
+    serialize(data, index) {
+      const { type } = data;
+      let html = "";
+
+      switch (type) {
+        case Elements.img.type:
+          html += this.serializeImage(data, index);
+          break;
+
+        case Elements.pre.type:
+          html += Elements.pre.open(data, this.classes);
+          break;
+
+        default:
+          html += this.serializeText(data, index);
+          break;
+      }
+
+      return html;
+    },
+    serializeText(data, index) {
+      let { text, spans, type } = data;
+      let html = "";
+      let currentIndex = 0;
+
+      const { open, close } = this.findElement(type);
+
+      html += this.processOpeningWrappers(type);
+      html += open({}, this.wrappingHtmlElementOpen ? "" : this.classes);
+
+      spans.forEach(span => {
+        const { type, data, start, end } = span;
+        const { open, close } = this.findElement(type);
+
+        html += text.slice(currentIndex, start);
+        currentIndex = start;
+        html += open(data);
+        html += text.slice(currentIndex, end);
+        currentIndex = end;
+        html += close();
+      });
+
+      html += text.slice(currentIndex, text.length);
+
+      html += close();
+      html += this.processClosingWrappers(type, index);
+
+      return html;
+    },
+    serializeImage(data) {
+      const { type, alt, dimensions, copyright, url } = data;
+      const { open, close } = Elements.img;
+
+      return open({}, this.classes, alt, url) + close();
+    },
+    processOpeningWrappers(type) {
+      let el;
+
+      if (!this.ulListOpen && type === Elements.ulli.type) {
+        el = Elements.ul;
+        this.ulListOpen = true;
+      } else if (!this.olListOpen && type === Elements.olli.type) {
+        el = Elements.ol;
+        this.olListOpen = true;
+      } else {
+        return "";
+      }
+
+      const wrappers = el.open({}, this.classes);
+
+      return wrappers;
+    },
+    processClosingWrappers(type, index) {
+      let el;
+
+      if (
+        this.olListOpen &&
+        (type !== Elements.olli.type || index === this.body.length - 1)
+      ) {
+        el = Elements.ol;
+        this.olListOpen = false;
+      } else if (
+        this.ulListOpen &&
+        (type !== Elements.ulli.type || index === this.body.length - 1)
+      ) {
+        el = Elements.ul;
+        this.ulListOpen = false;
+      } else {
+        return "";
+      }
+
+      const wrappers = el.close();
+
+      return wrappers;
+    }
+  }
+};
+</script>
--- src/views/Page.vue
+++ src/views/Page.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="c-page">
     <section
-      v-for="(section, index) in [1, 2]"
-      :key="`${index}-${uid}`"
+      v-for="([titleSlice, ...slices], index) in sections"
+      :key="`${title}-slice-${index}`"
       class="c-page__section"
     >
       <header class="c-page__section__header">
@@ -12,86 +12,18 @@
         ></header-gradient>
       </header>
 
-      <h1 class="c-page__section__title">{{ title }}</h1>
+      <!-- This design is highly coupled, so much so that can always use h1 instead of rich-text -->
+      <h1 class="c-page__section__title">
+        {{ titleSlice.primary.section_title | prismicRawText }}
+      </h1>
 
       <div class="c-page__section__content">
         <paper class="c-page__section__paper" :color="color">
-          <section>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Pellentesque quis lacus aliquet, mollis lacus et, sollicitudin
-              turpis. Vestibulum dapibus tincidunt urna, et varius justo
-              tincidunt vel. Sed sodales ut sem id efficitur. Vestibulum quis
-              ligula quis nunc ultrices suscipit. Aenean vestibulum purus
-              placerat, dictum orci et, scelerisque ante. Quisque imperdiet
-              dolor scelerisque luctus bibendum. Curabitur sed sapien sit amet
-              metus viverra efficitur. Nullam ac sagittis ipsum. Suspendisse
-              lorem neque, mollis nec ullamcorper vitae, consectetur a orci. Ut
-              interdum malesuada commodo. Sed et sollicitudin turpis. Proin a
-              egestas massa, vulputate egestas diam. Nulla risus dolor,
-              vulputate vitae turpis in, blandit semper nulla. Quisque ultrices
-              eget libero et tempor. Nulla mi diam, laoreet in est eget,
-              interdum bibendum massa. Praesent convallis blandit lectus quis
-              iaculis.
-            </p>
-            <p>
-              Aliquam fringilla diam vitae erat porta, vitae congue justo
-              hendrerit. Vivamus cursus tristique arcu, vehicula pulvinar justo
-              porttitor ac. Donec nec est et ligula tempor interdum ut at nunc.
-              Maecenas posuere libero et tellus dignissim bibendum. Donec non
-              metus lectus. Donec vel diam aliquet, pulvinar nisi varius,
-              vehicula velit. Pellentesque euismod elit eu sapien auctor
-              volutpat. Integer faucibus quis lectus ut iaculis. Fusce imperdiet
-              et nisi at interdum. Fusce condimentum neque eget tortor
-              pellentesque, at tempor metus sodales. Quisque enim magna, commodo
-              sit amet velit eget, interdum ultricies orci. Donec dignissim,
-              enim ac bibendum imperdiet, elit orci faucibus augue, eget
-              dignissim est est at nunc. Nulla faucibus felis a sodales
-              porttitor. Vestibulum nec purus eget libero porta lobortis id et
-              ante.
-            </p>
-          </section>
-
-          <section>
-            <h2>Secondary Title</h2>
-            <p>
-              Donec finibus mauris ante, ullamcorper molestie diam dictum id.
-              Vivamus sit amet pharetra nunc. Ut volutpat leo sit amet mauris
-              condimentum imperdiet. Suspendisse eu sodales lacus. Nulla
-              tristique orci leo, ac iaculis nisl euismod sit amet. Praesent
-              quis velit accumsan, dictum velit eu, sodales tellus. Vestibulum
-              ante ipsum primis in faucibus orci luctus et ultrices posuere
-              cubilia Curae; Donec pretium et tellus sit amet pulvinar.
-            </p>
-          </section>
-
-          <section>
-            <h2>Secondary Title</h2>
-            <p>
-              Maecenas iaculis venenatis elit id consectetur. Ut dignissim
-              sodales justo, ut tincidunt diam. Quisque pellentesque semper
-              ipsum, sit amet sollicitudin metus congue sit amet. Suspendisse
-              vel nisi mollis, eleifend eros et, gravida justo. Mauris faucibus
-              dolor nibh, et tempus est tristique vel. Quisque gravida quam vel
-              sollicitudin aliquet. Proin scelerisque, nunc id aliquam
-              malesuada, erat risus vulputate arcu, vitae scelerisque sapien ex
-              a dolor. Praesent ac urna nisl. Sed ex metus, tristique et elit
-              id, imperdiet gravida massa. Sed ut sagittis justo. Proin sed
-              velit vel sem faucibus cursus consectetur volutpat mi. Curabitur
-              sagittis luctus metus, molestie venenatis nisl elementum eu. Fusce
-              id consequat elit. Nullam a arcu vel sem laoreet eleifend. In
-              finibus cursus orci, sit amet mollis leo hendrerit a. Vivamus
-              posuere imperdiet facilisis.
-            </p>
-            <p>
-              Fusce vitae felis risus. Aliquam quis gravida dui, quis laoreet
-              nulla. Maecenas id varius nisi. Sed facilisis aliquam vulputate.
-              Nullam mattis turpis eu nisl fringilla, ut porttitor lectus
-              blandit. Nam ut augue sit amet purus facilisis blandit eget non
-              elit. Phasellus in pellentesque velit. Nulla molestie ac ipsum ac
-              tempus. Aliquam pellentesque placerat velit.
-            </p>
-          </section>
+          <rich-text
+            v-for="({ primary, slice_type }, sliceIndex) in slices"
+            :key="`section-${index}_slice-${sliceIndex}`"
+            :body="primary[slice_type]"
+          ></rich-text>
         </paper>
       </div>
     </section>
@@ -104,10 +36,12 @@ import BaseView from "./BaseView";
 
 import HeaderGradient from "@/components/HeaderGradient";
 
+import RichText from "@/components/prismic/RichText.vue";
+
 export default {
   name: "Page",
   extends: BaseView,
-  components: { HeaderGradient },
+  components: { HeaderGradient, RichText },
   data: function() {
     return {
       items: _.range(0, 5, 1),
@@ -119,6 +53,45 @@ export default {
       return this.rawData
         ? this.PrismicProcessor.getRawText(this.rawData["page_title"])
         : "";
+    },
+    slices: function() {
+      if (!this.rawData) return [];
+
+      return this.rawData.body[0].primary.text;
+    },
+    testSlice: function() {
+      if (!this.rawData) return [];
+
+      return [this.slices[0]];
+    },
+    sliceTypes: function() {
+      if (!this.rawData) return [];
+
+      return _.uniqBy(this.slices.map(slice => slice.type));
+    },
+    body: function() {
+      if (!this.rawData) return [];
+
+      return this.rawData.body;
+    },
+    sections: function() {
+      let sections = [];
+
+      let section = [];
+      this.body.forEach(({ primary, slice_type }) => {
+        if (slice_type === "section_title" && section.length > 0) {
+          sections.push(section);
+          section = [];
+        }
+
+        section.push({ primary, slice_type });
+      });
+
+      if (section.length > 0) {
+        sections.push(section);
+      }
+
+      return sections;
     }
   },
   created: async function() {

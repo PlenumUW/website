4610839594d3a92b7d6dabf7bb82482707df92c4
Jackson Baker Ryan <jackryan@uw.edu>
2019-09-25 18:52:25 -0700

    Add events home slice coming soon placeholder

---
+++ src/components/slices/home/EventsSlice.vue
@@ -0,0 +1,50 @@
+<template>
+  <home-slice class="c-events-slice" :color="bgColor">
+    <template #title>
+      <router-link to="/events">Events</router-link>
+    </template>
+
+    <template #content>
+      <div class="c-events-slice__contents">
+        <div class="c-events-slice__contents__message">Events Coming Soon</div>
+      </div>
+    </template>
+  </home-slice>
+</template>
+<script>
+import _ from "lodash";
+import HomeSlice from "./HomeSlice.vue";
+
+export default {
+  name: "EventsSlice",
+  components: {
+    HomeSlice
+  },
+  props: {
+    bgColor: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.c-events-slice {
+  position: relative;
+  font-family: $font-serif;
+
+  &__contents {
+    display: flex;
+    align-items: center;
+
+    min-height: 400px;
+    font-size: 2em;
+
+    &__message {
+      width: fit-content;
+      height: fit-content;
+      margin: auto;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <issue-slice class="home-slice slice" :bgColor="color" :issue="currentIssue"></issue-slice>
     <atlas-slice class="home-slice slice" :bgColor="color"></atlas-slice>
+    <events-slice class="home-slice slice" :bgColor="color"></events-slice>
   </div>
 </template>
 
@@ -10,11 +11,12 @@ import BaseView from "./BaseView";
 
 import IssueSlice from "@/components/slices/home/IssueSlice";
 import AtlasSlice from "@/components/slices/home/AtlasSlice";
+import EventsSlice from "@/components/slices/home/EventsSlice";
 
 export default {
   name: "Home",
   extends: BaseView,
-  components: { IssueSlice, AtlasSlice },
+  components: { IssueSlice, AtlasSlice, EventsSlice },
   computed: {
     currentIssue: function () {
       return this.rawData ? this.rawData.issue : undefined;
